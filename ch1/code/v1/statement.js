552a11b500ade1ac4c75de7d7990478298ed2672
moonjin-kim <ace39418@gmail.com>
2024-05-06 18:20:50 +0900

    ref: 계산하는 함수들 enrichPerformance로 변경

--- ch1/code/v1/statement.js
+++ ch1/code/v1/statement.js
@@ -1,16 +1,29 @@
 function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance);
     return renderPlainText(statementData, plays);
 }
+
+function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(result)
+    result.amount = amountFor(result);
+    return result;
+}
+
+//2. 공연 명 반환
+function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+}
+
 
 function renderPlainText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
 
     for (let perf of data.performances) {
         // 청구 내역을 출력
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience})석\n`;
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience})석\n`;
     }
 
     result += `총액: ${usd(totalAmount(invoice))}\n`;
@@ -21,7 +34,7 @@ function renderPlainText(data, plays) {
 function totalAmount(invoice) {
     let result = 0;
     for (let perf of invoice.performances) {
-        result += amountFor(perf);
+        result += perf.amount;
     }
 
     return result
@@ -54,11 +67,6 @@ function volumeCreditsFor(perf) {
     return result;
 }
 
-//2. 공연 명 반환
-function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-}
-
 /**
  * 1. 함수 추출 / 장르별 금액 계산하는 함수
  * @param {*} perf 
@@ -68,7 +76,7 @@ function playFor(aPerformance) {
 function amountFor(aPerformance) {
     let result = 0;
 
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
         case "tragedy": //비극
             result = 40000;
             if (aPerformance.audience > 30) {
@@ -83,7 +91,7 @@ function amountFor(aPerformance) {
             result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+            throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
     }
 
     return result;
