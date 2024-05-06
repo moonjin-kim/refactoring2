90fd315c8fff7372340531a53bc5c2b162b1b8df
moonjin-kim <ace39418@gmail.com>
2024-05-06 17:35:50 +0900

    ref: amountFor 함수 inline

--- ch1/code/v1/statement.js
+++ ch1/code/v1/statement.js
@@ -7,15 +7,13 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf.playID)); // 1. 함수 추출 
-
         // 포인트 적립
         volumeCredits += Math.max(perf.audience - 30, 0);
         // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" == playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience})석\n`;
-        totalAmount += thisAmount;
+        result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience})석\n`;
+        totalAmount += amountFor(perf);
     }
 
     //청구 내역을 출력한다
@@ -25,8 +23,8 @@ function statement(invoice, plays) {
 }
 
 //2. 공연 명 반환
-function playFor(playId) {
-    return plays[playId];
+function playFor(aPerformance) {
+    return plays[aPerformance.playID];
 }
 
 /**
@@ -35,10 +33,10 @@ function playFor(playId) {
  * @param {*} aPerformance 
  * @returns thismount : int
  */
-function amountFor(aPerformance, play) {
+function amountFor(aPerformance) {
     let result = 0;
 
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
         case "tragedy": //비극
             result = 40000;
             if (aPerformance.audience > 30) {
@@ -53,7 +51,7 @@ function amountFor(aPerformance, play) {
             result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
+            throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
     }
 
     return result;
@@ -79,10 +77,10 @@ const invoiceDatas = [
     }
 ]
 
-const playData = {
+const plays = {
     "hamlet" : {"name" : "Hamlet" , "type": "tragedy"},
     "as-like" : {"name": "As You Like It", "type" : "comedy"},
     "othello": {"name": "Othello", "type": "tragedy"}
 };
 
-console.log(statement(invoiceDatas[0],playData))
+console.log(statement(invoiceDatas[0],plays))
