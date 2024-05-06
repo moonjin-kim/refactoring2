b21c539352b689c46089779abcbe6c524bf331bb
moonjin-kim <ace39418@gmail.com>
2024-05-06 18:12:18 +0900

    ref: ch1 v2 invoice 데이터 data로 옮기기

--- ch1/code/v1/statement.js
+++ ch1/code/v1/statement.js
@@ -1,13 +1,14 @@
 function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
-    return renderPlainText(statementData, invoice, plays);
+    statementData.performances = invoice.performances;
+    return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         // 청구 내역을 출력
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience})석\n`;
     }
