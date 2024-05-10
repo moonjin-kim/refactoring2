8276bd9149d5fce125d98f72bca9f848d2fc1c8f
moonjin-kim <ace39418@gmail.com>
2024-05-10 23:26:37 +0900

    ref: ch6 detail함수 추출

--- ch6/v0/printOwing.js
+++ ch6/v0/printOwing.js
@@ -1,25 +1,33 @@
 function printOwing(invoice) {
-    let outstanding = 0;
-
     //1. banner 출력 함수 추출
     printBanner();
 
-    //미해결 채무(outstanding) 계산
-    for (const o of invoice.orders) {
-        outstanding += o.amount;
-    }
+    //2. 채무 계산 함수 추출
+    let outstanding = calOutstanding(invoice)
 
     const today = Clock.today;
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-    console.log(`고객명: ${invoice.customer}`);
-    console.log(`채무액: ${outstanding}`);
-    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+    printDetail(invoice, outstanding);
 }
 
+//미해결 채무(outstanding) 계산
+function calOutstanding(invoice) {
+    result = 0;
+    for (const o of invoice.orders) {
+        result += o.amount;
+    }
+    return result
+}
 
 function printBanner() {
     console.log('****************');
     console.log('*****고객 채무*****');
     console.log('****************');
+}
+
+function printDetail(invoice, outstanding) {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
 }
