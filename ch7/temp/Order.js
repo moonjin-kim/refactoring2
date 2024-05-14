cce51d66b76acf9f1dfb79a3f4be84e85cbfe838
moonjin-kim <ace39418@gmail.com>
2024-05-14 23:12:20 +0900

    fix: basePrice 임시변수 함수로 수정

--- ch7/temp/Order.js
+++ ch7/temp/Order.js
@@ -5,10 +5,13 @@ class Order {
     }
 
     get price() {
-        var basePrice = this._quantity * this._item.price;
         var discountFactor = 0.98;
 
-        if (basePrice > 1000) discountFactor -= 0.03;
-        return basePrice * discountFactor;
+        if (this.basePrice > 1000) discountFactor -= 0.03;
+        return this.basePrice * discountFactor;
+    }
+
+    get basePrice() {
+        return this._quantity * this._item.price;
     }
 }
