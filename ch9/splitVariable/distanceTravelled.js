925ac5a83fe69e50438da6e334ab50e4e3f353bb
moonjin-kim <ace39418@gmail.com>
2024-05-16 23:16:50 +0900

    add: discount

--- ch9/splitVariable/distanceTravelled.js
+++ ch9/splitVariable/distanceTravelled.js
@@ -1,13 +1,16 @@
 function distanceTravelled (scenario, time) {
-        let result;
-        const primaryAcceleration = scenario.primaryForce / scenario.mass; // 가속도(a) = 힘(F) / 질량(m)
-        let primaryTime = Math.min(time, scenario.delay);
-        result = 0.5 * primaryAcceleration * primaryTime;
-        let secondaryTime = time - scenario.delay;
-        if (secondaryTime > 0) {
-            let primaryVelocity = primaryAcceleration * scenario.delay;
-            const secondaryAcceleration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
-            result += primaryVelocity * secondaryTime + 0.5 * secondaryAcceleration * secondaryTime * secondaryTime;
-        }
-        return result;
+    //Primary acceleration 계산
+    let result;
+    const primaryAcceleration = scenario.primaryForce / scenario.mass; // 가속도(a) = 힘(F) / 질량(m)
+    let primaryTime = Math.min(time, scenario.delay);
+    result = 0.5 * primaryAcceleration * primaryTime;
+
+    //Secondary acceleration 계산
+    let secondaryTime = time - scenario.delay;
+    if (secondaryTime > 0) {
+        let primaryVelocity = primaryAcceleration * scenario.delay;
+        const secondaryAcceleration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+        result += (primaryVelocity  + 0.5 * secondaryAcceleration * secondaryTime) * secondaryTime;
+    }
+    return result;
 }
